1c7351a92f7e44a0eb77885850984d5cf42138a7
leon.cilliers <leon.cilliers@momentum.co.za>
2023-12-28 23:29:02 +0200

    Fix SDH on multiple lines and remove lone speaker dash after SDH has been removed

--- src/es/instructions/sdh.js
+++ src/es/instructions/sdh.js
@@ -1,5 +1,5 @@
 import {fontClose, fontTag, undefinedIsEmptyString} from '../_utils';
-import {COLORS} from '../_constants';
+import {COLORS, RX} from '../_constants';
 import {collapseMultilines} from './multiline';
 
 export function sdhInstructions(section, include_sdh) {
@@ -11,11 +11,22 @@ export function sdhInstructions(section, include_sdh) {
   // Re-balance multiline
   section = collapseMultilines(section);
 
+  // Fix SHD on multiple lines
+  // From
+  //   [BELL CHIMES%> <%SNOOPY WHINES]
+  // To
+  //   [BELL CHIMES]
+  //   [SNOOPY WHINES]
+  section.text = section.text.replace(/(%>) (<%)/g, '$1\n$2');
+
   if (include_sdh === true) {
-    section.text = section.text.replace(/<%(.*)%>/g, fontTag(COLORS.EFFECTS) + `[$1]` + fontClose);
+    section.text = section.text.replace(/<%([^<]+)%>/g, _sdhReplace);
   } else {
-    section.text = section.text.replace(/<%(.*)(?<!%>)%>/g, '');
+    section.text = section.text.replace(/<%([^<]+)%>/g, '');
   }
+
+  // Remove lone speaker dash after SDH was removed
+  section.text = section.text.replace(_loneSpeakerDashRegex, '');
 
   section.text = section.text.trim();
   if (section.text.length === 0)
@@ -28,3 +39,6 @@ const _prepSdhReplace = (match, p1, p2) => {
   p2 = (undefinedIsEmptyString(p2)).toUpperCase().trim();
   return `<%${p2}%>`;
 };
+
+const _loneSpeakerDashRegex = new RegExp(`(^|${RX.BREAK})- ${RX.BREAK}`, 'g');
+const _sdhReplace = fontTag(COLORS.EFFECTS) + `[$1]` + fontClose;
